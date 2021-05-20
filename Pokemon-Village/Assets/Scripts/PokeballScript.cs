3ad59fd4da0b8000cfeda079568504e4f28d51a7
SDAxium <sdaxium@gmail.com>
2021-05-20 14:20:07 -0400

    More Changes
    Added Lapras and copied pokemon around the forest to make it less empty

--- Pokemon-Village/Assets/Scripts/PokeballScript.cs
+++ Pokemon-Village/Assets/Scripts/PokeballScript.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class PokeballScript : MonoBehaviour
 {
+    private float x;
+    private float y;
+    private float z;
     public GameObject pokemonToSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
     }
 
     // Update is called once per frame
@@ -20,10 +26,10 @@ public class PokeballScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (other.gameObject.CompareTag("Player"))
         {
             print("colliding");
-            Instantiate(pokemonToSpawn, this.transform.position + new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            Instantiate(pokemonToSpawn, this.transform.position + new Vector3(x, y, z), Quaternion.identity);
         }
     }
 }
